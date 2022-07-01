00ea19dab25879157b07433926f10c7ec2fde472
Harold Bengter <Haroldbe55@yahoo.com>
2022-07-01 12:46:50 -0400

    Created variables

---
+++ Variables/Program.cs
@@ -0,0 +1,13 @@
+﻿string myName = "Ben"; //outputs Ben
+int age = 23;          //Outputs 23
+bool myCheck = true;   //true
+double cash = 1200.54; //Outputs 1200.54
+char a = 'A';          //Outputs A
+decimal b = 2.5m;      //Outputs 2.5
+Console.WriteLine($"Hello my name is {myName} I am {age} years old.I was born in {a} small town in Atlanta. I am {b} years older than my sister. I have {cash} in my bank account");
+
+
+
+
+
+bool check = false;
